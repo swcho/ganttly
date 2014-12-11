cdd93e688a8d63755ffcbd4819c521ec37051392
swcho <swcho@humaxdigital.com>
2014-12-11 10:57:06 +0900

    adjust schedule params

--- directive/dhxSchedule/dhxSchedule.ts
+++ directive/dhxSchedule/dhxSchedule.ts
@@ -17,7 +17,7 @@ angular.module('ganttly').directive('dhxSchedule', function () {
 
             console.log('dhxSchedule.js');
 
-            scheduler.config.readonly = false;
+            scheduler.config.readonly = true;
 
             scheduler.locale.labels.timeline_tab = "Timeline";
             scheduler.locale.labels.section_custom = "Section";
@@ -52,10 +52,10 @@ angular.module('ganttly').directive('dhxSchedule', function () {
                 name: "timeline",
                 x_unit: "day",
                 x_date: "%m/%d",
-                x_step: 1,
-                x_size: 32,
-                x_start: 16,
-                x_length: 48,
+//                x_step: 1,
+//                x_size: 20,
+//                x_start: 16,
+//                x_length: 20,
                 y_unit: sections,
                 y_property: "section_id",
                 render: "tree",
@@ -82,16 +82,16 @@ angular.module('ganttly').directive('dhxSchedule', function () {
                 { start_date: "2009-06-30 16:00", end_date: "2009-06-30 17:00", text: "Task A-46598", section_id: 7},
 
                 { start_date: "2009-06-30 12:00", end_date: "2009-06-30 20:00", text: "Task B-48865", section_id: 2},
-                { start_date: "2009-06-30 14:00", end_date: "2009-06-30 16:00", text: "Task B-44864", section_id: 2},
-                { start_date: "2009-06-30 16:30", end_date: "2009-06-30 18:00", text: "Task B-46558", section_id: 2},
-                { start_date: "2009-06-30 18:30", end_date: "2009-06-30 20:00", text: "Task B-45564", section_id: 2},
+//                { start_date: "2009-06-30 14:00", end_date: "2009-06-30 16:00", text: "Task B-44864", section_id: 2},
+//                { start_date: "2009-06-30 16:30", end_date: "2009-06-30 18:00", text: "Task B-46558", section_id: 2},
+//                { start_date: "2009-06-30 18:30", end_date: "2009-06-30 20:00", text: "Task B-45564", section_id: 2},
 
                 { start_date: "2009-06-30 08:00", end_date: "2009-06-30 12:00", text: "Task C-32421", section_id: 3},
-                { start_date: "2009-06-30 14:30", end_date: "2009-06-30 16:45", text: "Task C-14244", section_id: 3},
+//                { start_date: "2009-06-30 14:30", end_date: "2009-06-30 16:45", text: "Task C-14244", section_id: 3},
 
                 { start_date: "2009-06-30 09:20", end_date: "2009-06-30 12:20", text: "Task D-52688", section_id: 4},
-                { start_date: "2009-06-30 11:40", end_date: "2009-06-30 16:30", text: "Task D-46588", section_id: 4},
-                { start_date: "2009-06-30 12:00", end_date: "2009-06-30 18:00", text: "Task D-12458", section_id: 4}
+//                { start_date: "2009-06-30 11:40", end_date: "2009-06-30 16:30", text: "Task D-46588", section_id: 4},
+//                { start_date: "2009-06-30 12:00", end_date: "2009-06-30 18:00", text: "Task D-12458", section_id: 4}
             ], "json");
             scheduler.openAllSections();
 
