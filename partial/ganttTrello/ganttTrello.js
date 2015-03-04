d21bc07c38e72add663029f96496b0e464597d36
Sungwoo <sungwoo.cho.dev@gmail.com>
2015-03-04 14:17:04 +0900

    typescript generated

--- partial/ganttTrello/ganttTrello.js
+++ partial/ganttTrello/ganttTrello.js
@@ -4,25 +4,20 @@
 /// <reference path="../../lib/TrelloUtils.ts"/>
 angular.module('ganttly').controller('GantttrelloCtrl', function ($scope, $state, $stateParams) {
     var boardId = $stateParams.board;
-
     var context = new UiUtils.CAngularContext($scope);
-
     var cbBoard = new DhxExt.CCombo(document.getElementById('idCbBoard'));
     context.addComponent(cbBoard);
-
     cbBoard.onChange = function (id) {
         console.log('onChange', id);
         $state.go('ganttTrello', {
             board: id
         });
     };
-
     TrelloUtils.getBoards(function (err, boards) {
         if (err) {
             console.log(err);
             return;
         }
-
         var items = [];
         boards.forEach(function (b) {
             items.push({
@@ -30,39 +25,34 @@ angular.module('ganttly').controller('GantttrelloCtrl', function ($scope, $state
                 text: b.name
             });
         });
-
         cbBoard.setItems(items);
         cbBoard.selectItemById(boardId);
     });
-
     var gantt = new DhxExt.Gantt.CGantt(document.getElementById('idGantt'), true);
     context.addComponent(gantt);
-
     gantt.setContextMenu({
         menuItems: [{
-                id: 'open_card',
-                text: 'Open card',
-                onClick: function (id, param) {
-                    console.log(param);
-                    var task = gantt._gantt.getTask(param.taskId);
-                    var url = task._data.shortUrl;
-                    var width = 1280;
-                    var height = 720;
-                    var params = [
-                        'width=' + width,
-                        'height=' + height,
-                        'fullscreen=yes'
-                    ].join(',');
-                    var win = open(url, null, params);
-                    win.moveTo((screen.width - width) / 2, (screen.height - height) / 2);
-                    win.resizeTo(width, height);
-                }
-            }]
+            id: 'open_card',
+            text: 'Open card',
+            onClick: function (id, param) {
+                console.log(param);
+                var task = gantt._gantt.getTask(param.taskId);
+                var url = task._data.shortUrl;
+                var width = 1280;
+                var height = 720;
+                var params = [
+                    'width=' + width,
+                    'height=' + height,
+                    'fullscreen=yes'
+                ].join(',');
+                var win = open(url, null, params);
+                win.moveTo((screen.width - width) / 2, (screen.height - height) / 2);
+                win.resizeTo(width, height);
+            }
+        }]
     });
-
     if (boardId) {
         var s = [];
-
         var board;
         var memberIdList;
         s.push(function (done) {
@@ -80,7 +70,6 @@ angular.module('ganttly').controller('GantttrelloCtrl', function ($scope, $state
                 done(err);
             });
         });
-
         var mapMember;
         s.push(function (done) {
             TrelloUtils.getMembers(memberIdList, function (err, mm) {
@@ -88,7 +77,6 @@ angular.module('ganttly').controller('GantttrelloCtrl', function ($scope, $state
                 done(err);
             });
         });
-
         s.push(function (done) {
             var data = [];
             var links = [];
@@ -101,33 +89,25 @@ angular.module('ganttly').controller('GantttrelloCtrl', function ($scope, $state
                     duration: 1,
                     user: ''
                 };
-
                 if (card.due) {
                     task.end_date = new Date(card.due);
                 }
-
                 var users = [];
                 card.idMembers.forEach(function (mid) {
                     var name = mapMember[mid].fullName;
                     users.push(name);
                 });
-
                 task.user = users.join(',');
-
                 task._data = card;
-
                 data.push(task);
             });
-
             gantt.clearAll();
-
             gantt.parse({
                 data: data,
                 links: links
             });
             done();
         });
-
         async.series(s, function (err) {
         });
     }
